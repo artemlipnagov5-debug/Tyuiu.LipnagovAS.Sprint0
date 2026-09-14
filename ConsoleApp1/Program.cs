504f61581c77be139547b5e0598858622db22b5a
artemlipnagov5-debug <artemlipnagov5@gmail.com>
2026-09-14 11:48:50 +0500

    Task1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Артём";
+            int age = 18;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне "+ age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
