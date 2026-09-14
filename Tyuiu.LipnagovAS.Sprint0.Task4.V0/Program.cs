faaa10e91bce5f828150d46a777211f3f31d1b87
artemlipnagov5-debug <artemlipnagov5@gmail.com>
2026-09-14 20:04:03 +0500

    Task4

---
+++ Tyuiu.LipnagovAS.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.LipnagovAS.Sprint0.Task4.V0.Lib;
+
+
+namespace Tyuiu.LipnagovAS.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(1, 5));
+
+            Console.WriteLine(DataService.Subtraction(1, 5));
+
+            Console.WriteLine(DataService.Multiplication(1, 5));
+
+            Console.WriteLine(DataService.Division(1, 5));
+
+            Console.ReadKey();
+        }
+    }
+}
